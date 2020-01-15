f2a6193e31a4acf17f62e1f89177ef3774723fe8
Sanne Fujan <55358781+sannefujan@users.noreply.github.com>
2020-01-15 12:13:55 +0100

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+
+    background(0,0,0);
+    fill(33, 120, 65);
+    //ellipse(mouseX, mouseY,50, 50, 80, 80);
+
+    rect(0, 0, 600 / 2, 100);
+}
